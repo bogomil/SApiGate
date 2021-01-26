508a8e709ae1b3ef4e9467174e2ed042451a4b39
Bogomil Shopov <bogomil.shopov@citrix.com>
2021-01-26 12:00:06 +0100

    Changed the host

--- apigate.py
+++ apigate.py
@@ -21,4 +21,4 @@ def process_image():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port) 
+    app.run(host='sapigate.herokuapp.com', port=port) 
