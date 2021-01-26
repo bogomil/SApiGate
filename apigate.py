9f0cbd8c24ca1eee899de188f7b7a5b134458dd2
Bogomil Shopov <bogomil.shopov@citrix.com>
2021-01-26 11:27:39 +0100

    Secure API Gate initial commit

---
+++ apigate.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from PIL import Image
+from stegano import lsb
+import os
+
+
+app = Flask(__name__)
+
+@app.route("/steg", methods=["POST"])
+def process_image():
+    file = request.files['image']
+    try:
+        img = Image.open(file.stream)
+        msg ="sucess"
+        mmsg = lsb.reveal(img)
+    except:
+        msg = "error"
+        mmsg = "Something went wrong. Maybe you are not sending a picture"
+
+    return jsonify({'status': msg, 'message': mmsg})
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
